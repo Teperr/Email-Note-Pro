5b2c9ed9c6cf21cf1834eef4559d22939b6318cf
Idan Elfasi <idan60167@gmail.com>
2024-05-29 00:04:45 +0300

    pages links only basic render header and menu optiones

--- apps/mail/views/trashPage.jsx
+++ apps/mail/views/trashPage.jsx
@@ -1,3 +1,25 @@
+const { Link } = ReactRouterDOM
+const { useState, useEffect } = React
+const { useParams, useNavigate } = ReactRouter
+import { mailService } from "../services/mail.service.js"
+import { MailHeader } from "../cmps/mailAppHeader.jsx"
+import { MenuOptions } from "../cmps/menuOptionsMail.jsx"
+import { MailPages } from "./mailPages.jsx"
+import { EmailCompose } from "../cmps/EmailCompose.jsx"
+
 export function TrashPage(){
-    return <h2>trash</h2>
+    return (
+        <section>
+            <div className="main-container">
+            <MailHeader/>
+    
+                <main className="main-content">
+                    <MenuOptions />
+                    {/* <MailPages mails={mails} onRemove={removeMail}/> */}
+                </main>
+           <EmailCompose/> 
+            </div>
+        </section>
+    
+        )
 }
