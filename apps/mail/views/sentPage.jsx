e1e3cb24fe93dde6021098c81ab4ae613a58439e
Idan Elfasi <idan60167@gmail.com>
2024-05-31 16:26:07 +0300

    sent Page mails and connect to new mail form

--- apps/mail/views/sentPage.jsx
+++ apps/mail/views/sentPage.jsx
@@ -6,18 +6,38 @@ import { MailHeader } from "../cmps/mailAppHeader.jsx"
 import { MenuOptions } from "../cmps/menuOptionsMail.jsx"
 import { MailPages } from "./mailPages.jsx"
 import { EmailCompose } from "../cmps/EmailCompose.jsx"
+import { EmailFilter } from "../cmps/EmailFilter.jsx"
 
 export function SentPage(){
+
+    const [sentMails,setSentMails]=useState([])
+    const [filterBy, setFilterBy] = useState({ body: '' })
+
+    useEffect(() => {
+        mailService.query(filterBy)
+            .then(mails =>{
+            const sents=  mails.filter(mail=>(mail.isSent)) 
+                setSentMails(sents)
+                console.log('sents:', sents)
+              
+            })
+  
+}, [filterBy])
+
+
+    function OnsetFilterBy(newFilter) {
+        setFilterBy(newFilter)
+    }
+
     return (
         <section>
             <div className="main-container">
-            <MailHeader/>
+            <EmailFilter filterBy={filterBy} onFilter={OnsetFilterBy}/>
     
                 <main className="main-content">
                     <MenuOptions />
-                    {/* <MailPages mails={mails} onRemove={removeMail}/> */}
+                    <MailPages mails={sentMails} />
                 </main>
-           <EmailCompose/> 
             </div>
         </section>
     
